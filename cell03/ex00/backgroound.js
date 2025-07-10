3eaba74405d61532c63539de93877b6ddd25c1f4
Retr02k <retr0@Retro.localdomain>
2025-07-10 22:56:44 +0100

    cell03/ex00 complete

--- cell03/ex00/backgroound.js
+++ cell03/ex00/backgroound.js
@@ -1,4 +1,13 @@
-function get_random_num(r, g, b) {
+document.getElementById('colorBtn')
+.addEventListener('click', changeBackgroundColor)
 
+function get_random_num(max) {
+	return (Math.floor(Math.random() * max));
+}
 
+function changeBackgroundColor() {
+	const r = get_random_num(256);
+	const g = get_random_num(256);
+	const b = get_random_num(256);
+	document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
 }
